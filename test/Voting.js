37873b67bc68601203be648204d2dd47c1b9c112
Raldo CHEA <raldo.chea@gmail.com>
2022-04-05 13:33:22 +0100

    First commit

---
+++ test/Voting.js
@@ -0,0 +1,139 @@
+const Voting = artifacts.require("./Voting.sol");
+const { BN, expectRevert, expectEvent } = require('@openzeppelin/test-helpers');
+const { expect } = require('chai');
+
+contract("Voting", accounts => {
+  const owner = accounts[0];
+  let votingInstance;
+  describe("Test Voting smart contract to Alyra", function () {
+
+    before(async () => {
+      votingInstance = await Voting.new({from:owner});
+      await votingInstance.addVoter(owner, {from: owner});
+    });
+
+    // onlyOwner : je ne teste pas ce modifier vu qu'il vient d'une société reconnu par l'échosystème et est utilisé par de nombreux projet
+    // onlyVoters : je pense qu'il n'est pas nécessaire de faire ce test pour chaque méthode qui utilise ce modifier
+    it('no voter cannot use onlyVoters method', async function () {
+      await expectRevert(
+        votingInstance.getVoter(accounts[2], { from: accounts[2] }),
+        "You're not a voter",
+      );
+    });
+
+    // getVoter
+    it("...should get exist voter.", async () => {
+      await votingInstance.addVoter(accounts[1], {from: owner});
+      let voter = await votingInstance.getVoter.call(accounts[1], {from: owner});
+      expect(voter).exist;
+    });
+    
+    // addVoter
+    it("...bad workflow status.", async () => {
+      expectRevert(
+        votingInstance.addVoter(accounts[3], { from: accounts[2] }),
+        "Ownable: caller is not the owner",
+      );
+    });
+    
+    it("...voter already registed.", async () => {
+      expectRevert(
+        votingInstance.addVoter(owner, { from: owner }),
+        "Already registered",
+      );
+    });
+
+    it("...voter is registered.", async () => {
+      await votingInstance.addVoter(accounts[2], {from: owner});
+      let voter = await votingInstance.getVoter.call(accounts[2], {from: owner});
+      expect(voter.isRegistered).equal(true);
+    });
+
+    it("...emit VoterRegisted", async () => {
+      expectEvent(
+        await votingInstance.addVoter(accounts[3], {from: owner}),
+        "VoterRegistered",
+        {voterAddress: accounts[3]}
+      );
+    });
+
+    // addProposal
+    it("...bad workflow status.", async () => {
+      expectRevert(
+        votingInstance.addProposal('proposition 1', {from: owner}),
+        "Proposals are not allowed yet",
+      );
+    });
+    
+    it("...new proposal saved.", async () => {
+      await votingInstance.startProposalsRegistering({from: owner});
+      expectEvent(
+        await votingInstance.addProposal('proposition2', {from: owner}),
+        "ProposalRegistered",
+        {proposalId: new BN(0)}
+      );
+    });
+
+    it("...proposal cant be empty.", async () => {
+      expectRevert(
+        votingInstance.addProposal('', {from: owner}),
+        "Vous ne pouvez pas ne rien proposer",
+      )
+    });
+/*
+  it("...should get exist proposal.", async () => {
+    await votingInstance.startProposalsRegistering.call({from: owner});
+    await votingInstance.addProposal.call('proposition2', {from: owner});
+    let proposal = await votingInstance.getOneProposal.call(0, {from: owner});
+    expect(proposal).exist;
+  })
+
+  //it("...new proposal description save.", async () => {});
+  */
+  // getOneProposal
+  
+  /*
+  it("...proposal not found.", async () => {});
+  
+    
+  
+    // setVote
+    it("...bad workflow status.", async () => {});
+    it("...user already voted.", async () => {});
+    it("...proposal vote not found.", async () => {});
+    it("...vote registered.", async () => {});
+    it("...voter has voted.", async () => {});
+    it("...proposal vote growth up.", async () => {});
+    it("...vote is log.", async () => {});
+  
+    // startProposalsRegistering
+    it("...only owner can start proposal registering.", async () => {});
+    it("...owner start proposal registering.", async () => {});
+    it("...start proposal registering is log.", async () => {});
+  
+    // endProposalsRegistering
+    it("...only owner can end proposal registering.", async () => {});
+    it("...owner end proposal registering.", async () => {});
+    it("...end proposal registering is log.", async () => {});
+  
+    // startVotingSession
+    it("...only owner can start voting session.", async () => {});
+    it("...owner start voting session.", async () => {});
+    it("...start voting session is log.", async () => {});
+  
+    // endVotingSession
+    it("...only owner can end voting session.", async () => {});
+    it("...owner end voting session.", async () => {});
+    it("...end voting session is log.", async () => {});
+  
+    // tallyVotes
+    it("...voting session is not finished.", async () => {});
+    it("...tally votes is log.", async () => {});
+    it("...workflow status change to votes tallied");
+    it("...workflow status change to votes tallied");
+    */
+
+  // end describe  
+  });
+// end contracts  
+});
